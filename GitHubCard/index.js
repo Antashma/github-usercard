4812ed76f9c4482b973fffb98b9da5b6fd7a386f
Samantha Goodman <slgoodman89@gmail.com>
2020-07-19 22:16:57 -0400

    started step 3

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,19 @@
+import axios from 'axios';
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+axios.get('https://api.github.com/users/antashma')
+  .then(
+    (success) =>
+    console.log('axios request sucessful!', success)
+  )
+  .catch(
+    (fail) =>
+    console.log('axios request failed!', fail)
+  )
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
